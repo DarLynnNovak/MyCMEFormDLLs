9d9b3d0cf6b4f41752d0d2ffb49821c2e49c93d2
Edward Johnson <ejohnson@facs.org>
2020-09-03 07:42:57 -0500

    Initial Commit

---
+++ MyCME/FormLayoutControls/SendToBroker/SendCE.cs
@@ -0,0 +1,123 @@
+﻿using Aptify.Framework.Application;
+using Aptify.Framework.DataServices;
+using Aptify.Framework.ExceptionManagement;
+using Aptify.Framework.WindowsControls;
+using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ACSMyCME.FormLayoutControls.SendToBroker
+{ 
+   public class SendCE : FormTemplateLayout
+    {
+        private AptifyApplication m_oApp = new AptifyApplication();
+        private DataAction m_oda = new DataAction();
+        private AptifyProperties m_oProps = new AptifyProperties();
+
+        private AptifyLinkBox _senderIdLinkBox;
+        private AptifyActiveButton _sendToBrokerBtn;
+        private AptifyTextBox _eventStartDate;
+        private AptifyTextBox _eventEndDate;
+        string senderIdSql;
+        int senderId;
+        long userId;
+
+        public void Config()
+        {
+            try
+            {
+                m_oApp = ApplicationObject;
+                this.m_oda = new Aptify.Framework.DataServices.DataAction(this.m_oApp.UserCredentials);
+                userId = m_oda.UserCredentials.AptifyUserID;
+            }
+            catch (Exception ex)
+            {
+                Aptify.Framework.ExceptionManagement.ExceptionManager.Publish(ex);
+            }
+        }
+
+        protected override void OnFormTemplateLoaded(FormTemplateLoadedEventArgs e)
+        {
+            base.OnFormTemplateLoaded(e);
+            try
+            {
+                if (e.Operation == FormTemplateLoadedOperation.LoadTemplate)
+                {
+                    Config();
+                    BindControls();
+                    getSenderId();
+                   
+                }
+                
+                //   _sendToBrokerBtn.Click += _sendToBrokerBtn_Click;
+            }
+        
+            catch (Exception ex)
+            {
+                ExceptionManager.PublishAndDisplayException(this, ex);
+            }
+        }//End OnFormTemplateLoaded\
+
+
+        protected virtual void BindControls()
+        {
+            try
+            {
+                if (_sendToBrokerBtn == null || _sendToBrokerBtn.IsDisposed)
+                {
+                    _sendToBrokerBtn = GetFormComponentByLayoutKey(this, "ACS.ACSCMESendToBroker.Form.Active Button.1") as AptifyActiveButton;
+                }
+                if (_senderIdLinkBox == null || _senderIdLinkBox.IsDisposed)
+                {
+                    _senderIdLinkBox = GetFormComponentByLayoutKey(this, "ACS.ACSCMESendToBroker.Form.SenderId") as AptifyLinkBox;
+                }
+                if (_eventStartDate == null || _eventStartDate.IsDisposed)
+                {
+                    _eventStartDate = GetFormComponentByLayoutKey(this, "ACSCMESendToBroker.EventStartDate") as AptifyTextBox;
+                }
+                if (_eventEndDate == null || _eventEndDate.IsDisposed)
+                {
+                    _eventEndDate = GetFormComponentByLayoutKey(this, "ACSCMESendToBroker.EventEndDate") as AptifyTextBox;
+                }
+                _sendToBrokerBtn.Click += _sendToBrokerBtn_Click;
+
+
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.Publish(ex);
+            }
+        }//End Bind Controls
+        private void getSenderId()
+        {
+            if (userId != 11)
+            { 
+            senderIdSql = "select e.linkedpersonid from vwUserEntityRelations uer join vwemployees e on e.id = uer.EntityRecordID join vwusers u on u.id = uer.userid where u.id = " + userId;
+            senderId = Convert.ToInt32(m_oda.ExecuteScalar(senderIdSql));
+
+            }
+        }
+        void _sendToBrokerBtn_Click(object sender, System.EventArgs e)
+        {
+            CreateXml();
+            MessageBox.Show("Hello Julie ");
+
+
+        }//End BtnClick
+
+        private void CreateXml()
+        {
+            XDocument xDoc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("Courses",
+                    new XAttribute("id_parent_provider", "2"),
+                    new XAttribute("upload_key", "1234"),
+                    new XElement("Course")
+                    )
+                );
+            MessageBox.Show(Convert.ToString(xDoc));
+        }
+
+    }//End Class
+
+}//End Namespace
