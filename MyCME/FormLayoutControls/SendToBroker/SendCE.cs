0f14e029fc39b1fc1f8e02ae02988ca41ace89b0
Dar <dar@facs.org>
2020-09-09 19:20:41 -0500

    Saving xml to the xml data field.  Started code to create attachment.

--- MyCME/FormLayoutControls/SendToBroker/SendCE.cs
+++ MyCME/FormLayoutControls/SendToBroker/SendCE.cs
@@ -2,26 +2,41 @@
 using Aptify.Framework.DataServices;
 using Aptify.Framework.ExceptionManagement;
 using Aptify.Framework.WindowsControls;
+using Aptify.Framework.BusinessLogic.GenericEntity;
 using System;
+using System.IO;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using System.Xml;
+using System.Web;
 
 namespace ACSMyCMEFormDLLs.FormLayoutControls.SendToBroker
 { 
    public class SendCE : FormTemplateLayout
     {
-        private AptifyApplication m_oApp = new AptifyApplication();
+
         private DataAction m_oda = new DataAction();
         private AptifyProperties m_oProps = new AptifyProperties();
+        private AptifyApplication m_oApp = new AptifyApplication();
+        private HttpPostedFileBase file;
 
+        private AptifyGenericEntityBase AttachmentsGE;
         private AptifyLinkBox _senderIdLinkBox;
         private AptifyActiveButton _sendToBrokerBtn;
         private AptifyTextBox _eventStartDate;
         private AptifyTextBox _eventEndDate;
+        XDocument xDoc = new XDocument();
+        string saveLocation = "C:/Code CSharp/ACSMyCMEFormDLLs/MyCME/FormLayoutControls/XML/XMLCEData.xml";
+        string attachmentCatIdSql;
+        int attachmentCatId;
+        string entityIdSql;
+        int entityId;
         string senderIdSql;
         int senderId;
         long userId;
-
+        long recordId;
+        string filename = null;
+        string result = "Failed";
         public void Config()
         {
             try
@@ -80,7 +95,7 @@ namespace ACSMyCMEFormDLLs.FormLayoutControls.SendToBroker
                     _eventEndDate = GetFormComponentByLayoutKey(this, "ACSCMESendToBroker.EventEndDate") as AptifyTextBox;
                 }
                 _sendToBrokerBtn.Click += _sendToBrokerBtn_Click;
-
+                recordId = FormTemplateContext.GE.RecordID;
 
             }
             catch (Exception ex)
@@ -102,20 +117,69 @@ namespace ACSMyCMEFormDLLs.FormLayoutControls.SendToBroker
             CreateXml();
             MessageBox.Show("Hello Julie ");
 
-
+           // CreateAttachment();
         }//End BtnClick
 
         private void CreateXml()
         {
-            XDocument xDoc = new XDocument(
+            xDoc = new XDocument(
                 new XDeclaration("1.0", "utf-8", "yes"),
                 new XElement("Courses",
-                    new XAttribute("id_parent_provider", "2"),
+                    new XAttribute("id_parent_provider", "2"), 
                     new XAttribute("upload_key", "1234"),
                     new XElement("Course")
                     )
                 );
             MessageBox.Show(Convert.ToString(xDoc));
+            xDoc.Save(saveLocation);
+            this.FormTemplateContext.GE.SetValue("XmlData", Convert.ToString(xDoc));
+            this.FormTemplateContext.GE.Save();
+        }
+       
+        private void CreateAttachment()
+        {
+            entityIdSql = "select ID from Entities where name like 'ACSCMESendToBroker'";
+            entityId = Convert.ToInt32(m_oda.ExecuteScalar(entityIdSql));
+            attachmentCatIdSql = "select ID from vwAttachmentCategories where name like 'MyCMEXML'";
+            attachmentCatId = Convert.ToInt32(m_oda.ExecuteScalar(attachmentCatIdSql));
+            //need to get the file that gets created and read it back into
+        
+            filename = Path.GetFileName(saveLocation);
+
+            byte[] array = File.ReadAllBytes(saveLocation);
+            var lTemp = new byte[array.Length];
+            
+            
+            //file.InputStream.Read(lTemp, 0, array.Length);
+
+            MessageBox.Show("Filename = " + filename);
+            MessageBox.Show("fileBytes = " + array.Length);
+
+            AttachmentsGE = m_oApp.GetEntityObject("Attachments", -1);
+            AttachmentsGE.SetValue("Name", "XMLCEData");
+            AttachmentsGE.SetValue("Description", "XMLCEData");
+            AttachmentsGE.SetValue("EntityID", entityId);
+            AttachmentsGE.SetValue("RecordID", recordId);
+            AttachmentsGE.SetValue("CategoryID", attachmentCatId);
+            AttachmentsGE.SetValue("LocalFileName", saveLocation);
+            AttachmentsGE.SetValue("BlobData", lTemp);
+             
+
+            if (!AttachmentsGE.Save(false))
+            {
+                result = "Error";
+                throw new Exception("Problem Saving attachments Record:" + AttachmentsGE.RecordID);
+
+            }
+            else
+            {
+                AttachmentsGE.Save(true);
+                result = "Success";
+
+            }
+
+
+
         }
 
     }//End Class
